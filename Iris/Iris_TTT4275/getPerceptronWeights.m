db4582976217174ae2e0e1504e9aa74a63ded82f
Harald Minde Hansen <haralmha@stud.ntnu.no>
2020-04-25 14:13:02 +0200

    Done some debugging and made getPerceptronWeights binary class

--- Iris/Iris_TTT4275/getPerceptronWeights.m
+++ Iris/Iris_TTT4275/getPerceptronWeights.m
@@ -1,20 +1,22 @@
-function weightMatrix = getPerceptronWeights(trainingData, trainingLabels)
+function W = getPerceptronWeights(trainingData, trainingLabels)
     numFeatures = size(trainingData,1);
-    numClasses = size(trainingLabels, 1);
-    alpha = 0.01;
+    alpha = 1;
     maxIter = 10000;
-    weightMatrix = zeros(numClasses, numFeatures+1); % W = [W, w_o]
+    W = zeros(2, numFeatures+1); % W = [W, w_o]
+    g = W*[trainingData;ones(1,size(trainingData,2))];
     iter = 0;
-    epsilon = 0.001;
-    mse = getMSE(weightMatrix*trainingData, trainingLabels);
-    while iter < maxIter || mse > epsilon
-        weightMatrix = weightMatrix - alpha*gradMSE(weightMatrix*trainingData, trainingLabels, trainingData);
+    epsilon = 0.01;
+    mse = getMSE(g, trainingLabels);
+    while iter < maxIter && mse > epsilon
+        W = W - alpha*gradMSE(g, trainingLabels, [trainingData;ones(1,size(trainingData,2))]);
+        g = W*[trainingData;ones(1,size(trainingData,2))];
         iter = iter + 1;
-        mse = getMSE(weightMatrix*trainingData, trainingLabels);
+        mse = getMSE(g, trainingLabels);
     end
     if iter >= maxIter
-        fprintf("Maximum number of iterations reached.");
+        fprintf("Maximum number of iterations reached.\n");
+        fprintf("MSE = %d\n", mse);
     else
-        fprintf("MSE converged.");
+        fprintf("MSE converged.\n");
     end
 end
