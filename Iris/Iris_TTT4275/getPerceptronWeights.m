184c2a65049eceacba155544ec3fffe7d5dabcf9
Harald Minde Hansen <haralmha@stud.ntnu.no>
2020-04-25 17:19:25 +0200

    Changed g and t vectors to scalars

--- Iris/Iris_TTT4275/getPerceptronWeights.m
+++ Iris/Iris_TTT4275/getPerceptronWeights.m
@@ -2,7 +2,7 @@ function W = getPerceptronWeights(trainingData, trainingLabels)
     numFeatures = size(trainingData,1);
     alpha = 0.01;
     maxIter = 10000;
-    W = zeros(2, numFeatures+1); % W = [W, w_o]
+    W = zeros(1, numFeatures+1); % W = [W, w_o]
     x = [trainingData;ones(1,size(trainingData,2))];
     g = sigmoid(W,x);
     iter = 0;
--- Iris/Iris_TTT4275/perceptronTest.m
+++ Iris/Iris_TTT4275/perceptronTest.m
@@ -10,32 +10,25 @@ testSamples = totSamples - trainingSamples;
 
 
 trainingData = zeros(numFeatures, 3*trainingSamples);
-trainingLabels = zeros(numClasses, 3*trainingSamples);
 for i = 1:trainingSamples
     trainingData(:,i) = x1all(i,:)';
     trainingData(:,trainingSamples+i) = x2all(i,:)';
     trainingData(:,2*trainingSamples+i) = x3all(i,:)';
-    trainingLabels(:,i) = [1;0;0];
-    trainingLabels(:,trainingSamples+i) = [0;1;0];
-    trainingLabels(:,2*trainingSamples+i) = [0;0;1];
 end
 testData = zeros(numFeatures, 3*testSamples);
-testLabels = zeros(numClasses, 3*testSamples);
+testLabels = zeros(1, 3*testSamples);
 for i = 1:testSamples
     testData(:,i) = x1all(trainingSamples+i,:)';
     testData(:,2*testSamples+i) = x2all(trainingSamples+i,:)';
     testData(:,3*testSamples+i) = x3all(trainingSamples+i,:)';
-    testLabels(:,i) = [1;0;0];
-    testLabels(:,testSamples+i) = [0;1;0];
-    testLabels(:,2*testSamples+i) = [0;0;1];
+    testLabels(:,i) = 1;
+    testLabels(:,testSamples+i) = 2;
+    testLabels(:,2*testSamples+i) = 3;
 end
 
-trainingLabels1 = [ones(1,trainingSamples),zeros(1,2*trainingSamples);
-                   zeros(1,trainingSamples),ones(1,2*trainingSamples)];
-trainingLabels2 = [zeros(1,trainingSamples),ones(1,trainingSamples),zeros(1,trainingSamples);
-                   ones(1,trainingSamples),zeros(1,trainingSamples),ones(1,trainingSamples)];
-trainingLabels3 = [zeros(1,2*trainingSamples),ones(1,trainingSamples);
-                   ones(1,2*trainingSamples),zeros(1,trainingSamples)];
+trainingLabels1 = [ones(1,trainingSamples),zeros(1,2*trainingSamples)];
+trainingLabels2 = [zeros(1,trainingSamples),ones(1,trainingSamples),zeros(1,trainingSamples)];
+trainingLabels3 = [zeros(1,2*trainingSamples),ones(1,trainingSamples)];
 
 weights1 = getPerceptronWeights(trainingData, trainingLabels1);
 weights2 = getPerceptronWeights(trainingData, trainingLabels2);
