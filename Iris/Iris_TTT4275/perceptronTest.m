cd156c1a252fcb7203f7625a2aaa56d19f17f822
Peter Hatlebrekke Husebø <peterhhu@stud.ntnu.no>
2020-04-26 15:10:46 +0200

    perceptronTest now divides data set as 30/20 and 20/30

--- Iris/Iris_TTT4275/perceptronTest.m
+++ Iris/Iris_TTT4275/perceptronTest.m
@@ -8,69 +8,98 @@ totSamples = 50;
 trainingSamples = 30;
 testSamples = totSamples - trainingSamples;
 
-% Extracts the 30 first rows of the data set
 trainingData = zeros(3*trainingSamples, numFeatures);
 trainingLabels = zeros(3*trainingSamples,1);
-for i = 1:trainingSamples
-    trainingData(i,:) = x1all(i,:);
-    trainingData(trainingSamples+i,:) = x2all(i,:);
-    trainingData(2*trainingSamples+i,:) = x3all(i,:);
-    trainingLabels(i) = 1;
-    trainingLabels(trainingSamples+i) = 2;
-    trainingLabels(2*trainingSamples+i) = 3;
-end
 
-% Extracts the 20 last rows of the data set
 testData = zeros(3*testSamples, numFeatures);
 testLabels = zeros(3*testSamples,1);
-for i = 1:testSamples
-    testData(i,:) = x1all(trainingSamples+i,:);
-    testData(testSamples+i,:) = x2all(trainingSamples+i,:);
-    testData(2*testSamples+i,:) = x3all(trainingSamples+i,:);
-    testLabels(i) = 1;
-    testLabels(testSamples+i) = 2;
-    testLabels(2*testSamples+i) = 3;
-end
 
-% Three labels to use for the "one vs all" approach
-trainingLabels1 = [ones(trainingSamples,1);zeros(2*trainingSamples,1)];
-trainingLabels2 = [zeros(trainingSamples,1);ones(trainingSamples,1);zeros(trainingSamples,1)];
-trainingLabels3 = [zeros(2*trainingSamples,1);ones(trainingSamples,1)];
-
-% The weights consists of coefficients for three hyperplanes for separating
-% one class from the rest (weights1 separates class 1 from the two others,
-% weights2 separates class 2, and weights 3 separates class 3).
-weights1 = getPerceptronWeights(trainingData, trainingLabels1);
-weights2 = getPerceptronWeights(trainingData, trainingLabels2);
-weights3 = getPerceptronWeights(trainingData, trainingLabels3);
-
-% Classifies the test set based on the features of each test sample and
-% the weight coefficients for the three hyperplanes.
-classifiedTestLabels = perceptronClassification(weights1,weights2,weights3,testData);
-% Classifies the training set based on the features of each training sample and
-% the weight coefficients for the three hyperplanes.
-classifiedTrainingLabels = perceptronClassification(weights1,weights2,weights3,trainingData);
-
-
-testConfusionMatrix = zeros(numClasses);
-for i = 1:testSamples*numClasses
-    testConfusionMatrix(testLabels(i),classifiedTestLabels(i)) = testConfusionMatrix(testLabels(i),classifiedTestLabels(i))+1;
-end
-trainingConfusionMatrix = zeros(numClasses);
-for i = 1:trainingSamples*numClasses
-    trainingConfusionMatrix(trainingLabels(i),classifiedTrainingLabels(i)) = trainingConfusionMatrix(trainingLabels(i),classifiedTrainingLabels(i))+1;
-end
-fprintf("Confusion matrix for test set:\n");
-for i = 1:numClasses
-    for j = 1:numClasses
-        fprintf("%i\t", testConfusionMatrix(i,j));
+for k = 1:2
+    
+    if k == 1
+        fprintf('30 first for training, 20 last for testing\n');
+        % Extracts the 30 first rows of the data set into the training set
+        for i = 1:trainingSamples
+            trainingData(i,:) = x1all(i,:);
+            trainingData(trainingSamples+i,:) = x2all(i,:);
+            trainingData(2*trainingSamples+i,:) = x3all(i,:);
+            trainingLabels(i) = 1;
+            trainingLabels(trainingSamples+i) = 2;
+            trainingLabels(2*trainingSamples+i) = 3;
+        end
+        % Extracts the 20 last rows of the data set into the test set
+        for i = 1:testSamples
+            testData(i,:) = x1all(trainingSamples+i,:);
+            testData(testSamples+i,:) = x2all(trainingSamples+i,:);
+            testData(2*testSamples+i,:) = x3all(trainingSamples+i,:);
+            testLabels(i) = 1;
+            testLabels(testSamples+i) = 2;
+            testLabels(2*testSamples+i) = 3;
+        end
+    elseif k == 2
+        fprintf('\n\n30 last for training, 20 first for testing\n');
+        % Extracts the 30 last rows of the data set into the training set
+        for i = 1:trainingSamples
+            trainingData(i,:) = x1all(testSamples+i,:);
+            trainingData(trainingSamples+i,:) = x2all(testSamples+i,:);
+            trainingData(2*trainingSamples+i,:) = x3all(testSamples+i,:);
+            trainingLabels(i) = 1;
+            trainingLabels(trainingSamples+i) = 2;
+            trainingLabels(2*trainingSamples+i) = 3;
+        end
+        % Extracts the 20 first rows of the data set into the test set
+        for i = 1:testSamples
+            testData(i,:) = x1all(i,:);
+            testData(testSamples+i,:) = x2all(i,:);
+            testData(2*testSamples+i,:) = x3all(i,:);
+            testLabels(i) = 1;
+            testLabels(testSamples+i) = 2;
+            testLabels(2*testSamples+i) = 3;
+        end
     end
-    fprintf("\n");
-end
-fprintf("Confusion matrix for training set:\n");
-for i = 1:numClasses
-    for j = 1:numClasses
-        fprintf("%i\t", trainingConfusionMatrix(i,j));
+        
+
+    % Three labels to use for the "one vs all" approach
+    trainingLabels1 = [ones(trainingSamples,1);zeros(2*trainingSamples,1)];
+    trainingLabels2 = [zeros(trainingSamples,1);ones(trainingSamples,1);zeros(trainingSamples,1)];
+    trainingLabels3 = [zeros(2*trainingSamples,1);ones(trainingSamples,1)];
+
+    % The weights consists of coefficients for three hyperplanes for separating
+    % one class from the rest (weights1 separates class 1 from the two others,
+    % weights2 separates class 2, and weights 3 separates class 3).
+    weights1 = getPerceptronWeights(trainingData, trainingLabels1);
+    weights2 = getPerceptronWeights(trainingData, trainingLabels2);
+    weights3 = getPerceptronWeights(trainingData, trainingLabels3);
+
+    % Classifies the test set based on the features of each test sample and
+    % the weight coefficients for the three hyperplanes.
+    classifiedTestLabels = perceptronClassification(weights1,weights2,weights3,testData);
+    % Classifies the training set based on the features of each training sample and
+    % the weight coefficients for the three hyperplanes.
+    classifiedTrainingLabels = perceptronClassification(weights1,weights2,weights3,trainingData);
+
+
+    testConfusionMatrix = zeros(numClasses);
+    for i = 1:testSamples*numClasses
+        testConfusionMatrix(testLabels(i),classifiedTestLabels(i)) = testConfusionMatrix(testLabels(i),classifiedTestLabels(i))+1;
     end
-    fprintf("\n");
+    trainingConfusionMatrix = zeros(numClasses);
+    for i = 1:trainingSamples*numClasses
+        trainingConfusionMatrix(trainingLabels(i),classifiedTrainingLabels(i)) = trainingConfusionMatrix(trainingLabels(i),classifiedTrainingLabels(i))+1;
+    end
+    fprintf("Confusion matrix for test set:\n");
+    for i = 1:numClasses
+        for j = 1:numClasses
+            fprintf("%i\t", testConfusionMatrix(i,j));
+        end
+        fprintf("\n");
+    end
+    fprintf("Confusion matrix for training set:\n");
+    for i = 1:numClasses
+        for j = 1:numClasses
+            fprintf("%i\t", trainingConfusionMatrix(i,j));
+        end
+        fprintf("\n");
+    end
+
 end
