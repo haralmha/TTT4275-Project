f6ab2acc1425270317abd55e9e8f3ceb97eca2e2
Harald Minde Hansen <haralmha@stud.ntnu.no>
2020-04-26 12:28:15 +0200

    Transposed data and label matrices

--- Iris/Iris_TTT4275/getPerceptronWeights.m
+++ Iris/Iris_TTT4275/getPerceptronWeights.m
@@ -1,18 +1,18 @@
 function W = getPerceptronWeights(trainingData, trainingLabels)
-    numFeatures = size(trainingData,1);
+    numFeatures = size(trainingData,2);
     alpha = 0.01; % "Learning rate". How much change applied to W for each iteration
     maxIter = 10000;
     W = zeros(1, numFeatures+1); % W = [W, w_o]
-    x = [trainingData;ones(1,size(trainingData,2))]; % x = [x',1]' (see 3.2 in compendium)
+    x = [(trainingData');ones(1,size(trainingData,1))]; % x = [x',1]' (see 3.2 in compendium)
     g = sigmoid(W,x); % 0 < g < 1
     iter = 0;
     epsilon = 0.01; % Error threshold to break the loop if mse converges
-    mse = getMSE(g, trainingLabels); % Minimum square error
+    mse = getMSE(g, trainingLabels'); % Minimum square error
     while iter < maxIter && mse > epsilon
-        W = W - alpha*gradMSE(g, trainingLabels, x); % Update weight matrix W
+        W = W - alpha*gradMSE(g, trainingLabels', x); % Update weight matrix W
         g = sigmoid(W,x);
         iter = iter + 1;
-        mse = getMSE(g, trainingLabels);
+        mse = getMSE(g, trainingLabels');
     end
     if iter >= maxIter
         fprintf("Maximum number of iterations reached.\n");
--- Iris/Iris_TTT4275/perceptronClassification.m
+++ Iris/Iris_TTT4275/perceptronClassification.m
@@ -1,11 +1,11 @@
 function labels = perceptronClassification(weights1, weights2, weights3, testData)
-    testSamples = size(testData,2);
-    labels = zeros(1,testSamples);
-    g = zeros(1,3);
+    testSamples = size(testData,1);
+    labels = zeros(testSamples,1);
+    g = zeros(3,1);
     for i = 1:testSamples
-        g(:,1) = weights1(:,1:end-1)*testData(:,i) + weights1(:,end);
-        g(:,2) = weights2(:,1:end-1)*testData(:,i) + weights2(:,end);
-        g(:,3) = weights3(:,1:end-1)*testData(:,i) + weights3(:,end);
-        labels(1,i) = (find([g(:,1),g(:,2),g(:,3)] == max([g(:,1),g(:,2),g(:,3)])));
+        g(1) = weights1(:,1:end-1)*(testData(i,:)') + weights1(:,end);
+        g(2) = weights2(:,1:end-1)*(testData(i,:)') + weights2(:,end);
+        g(3) = weights3(:,1:end-1)*(testData(i,:)') + weights3(:,end);
+        labels(i) = (find([g(1),g(2),g(3)] == max([g(1),g(2),g(3)])));
     end
 end
