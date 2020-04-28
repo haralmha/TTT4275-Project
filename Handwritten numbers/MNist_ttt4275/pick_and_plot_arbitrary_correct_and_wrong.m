094f8cd4bf051147cfc3f54bcdc3765c97e8adda
Peter Hatlebrekke Husebø <peterhhu@stud.ntnu.no>
2020-04-28 15:22:47 +0200

    There the plotter was modified again, git(t)

--- Handwritten numbers/MNist_ttt4275/pick_and_plot_arbitrary_correct_and_wrong.m
+++ Handwritten numbers/MNist_ttt4275/pick_and_plot_arbitrary_correct_and_wrong.m
@@ -5,12 +5,18 @@ while ~found_correct || ~found_uncorrect
     if (classified_testlab(i) == testlab(i)) && ~found_correct
         fprintf('Index %i: Classified as %i --> Labeled as %i\n', i,classified_testlab(i), testlab(i))
         found_correct = true;
+        x = zeros(28,28);
+        x(:) = testv(i,:);
+        figure(1)
+        image(x')
+        title('Correctly classified')
     elseif (classified_testlab(i) ~= testlab(i)) && ~found_uncorrect
         fprintf('Index %i: Classified as %i --> Labeled as %i\n', i,classified_testlab(i), testlab(i))
         found_uncorrect = true;
+        x = zeros(28,28);
+        x(:) = testv(i,:);
+        figure(2)
+        image(x')
+        title('Misclassified')
     end
-    x = zeros(28,28);
-    x(:) = testv(i,:);
-    figure(i)
-    image(x)
 end
